7b9709f9dd4ea38cc6c9279781f8ff8ff0e2e9b9
tantely <tantelitiana22@gmail.com>
2019-06-15 19:32:04 +0200

    Model train with pre-trained Glove as embedding

---
+++ src/models/Glove_With_PreTrained_Model.py
@@ -0,0 +1,135 @@
+import numpy as np
+import pandas as pd
+import time
+from threading import Thread
+from sklearn.preprocessing import MinMaxScaler
+
+"""
+Ce fichier a pour usage d'entrainer un modèle avec un glove pre-entrainer
+"""
+
+Path_to_Glove_Trained = "../data/model_embedding_resum/glove.6B.300d.txt"
+DIMGLOVE=300
+
+
+model1 = [ ("clfMultinomialNB",MultinomialNB())]
+param1={"alpha":[0.5, 0.7, 0.9, 1.1, 1.3, 1.5]}
+model2 = [("clfLogistic",LogisticRegression())]
+param2={"C": [0.001,0.01,0.1,1,10,100,1000]}
+model3 = [("clfSVM",LinearSVC())]
+param3={"C": [0.1, 1, 10, 100, 1000]}
+model4 = [("clfrandomForest",RandomForestClassifier())]
+param4={"n_estimators": [200,500, 700]}
+model5 = [ ("GradientBoosting",GradientBoostingClassifier())]
+param5={"n_estimators": [20, 30, 40, 50, 60, 70, 80]}
+
+modelfinal=[(model1,param1),(model2,param2),(model3,param3),(model4,param4),(model5,param5)]
+
+
+
+def Embedd_vector():
+    
+    f = open(Path_to_Glove_Trained,"r")
+    dict_Glove={}
+    for x in f:
+        u = x.split()
+        dict_Glove[u[0]]=list(np.array(u[1:],dtype=np.float32))
+
+    return dict_Glove
+
+def transformX(X):
+    
+    Corresp= Embedd_vector()    
+    result = [np.mean([Corresp[u] for u in x.split() if u in Corresp.keys()],axis=0) for x in X]
+    
+    result = [x if not np.isnan(np.sum(x)) else np.zeros(self.vector_size) for x in result]
+
+    X_tranform = np.asarray(result)
+    scaler = MinMaxScaler()
+    scaler.fit(X_tranform)
+    XresMinMax=scaler.transform(X_tranform)
+    
+    return(XresMinMax)
+
+
+def trainModelGlove(X,Y,model,path,cv,n_jobs):
+
+    modele  = GridSearchCV(estimator = model[0][1], param_grid = param, cv=cv,n_jobs=n_jobs)
+    print("Model lauch:{}_{}".format(model,param))
+    t0=time.time()
+
+    X_transformed = transformX(X)
+    modele.fit(X_transformed,Y)
+    print("Model lauched successfull. Execution times:{}".format(time.time()-t0))
+    word_transformer = "Glove"
+    name_model = model[0][0]
+    modele_name = "model_{}_{}.sav".format(word_transformer,name_model)
+    pickle.dump( modele, open( self.path+modele_name, "wb" ) )
+
+ 
+
+
+
+
+
+class BestModelFinder(Thread):
+
+    """Model with threading to parallelize your luncher."""
+
+    def __init__(self,X,Y,ListParamModel,path,cv=5,n_jobs=2):
+        Thread.__init__(self)
+        self.X=X
+        self.Y=Y
+        self.ListParamModel = ListParamModel
+        self.path=path
+        self.cv=cv
+        self.n_jobs=n_jobs
+
+
+    def run(self):
+        cleardata=Cleardataset()
+        for mod,par in self.ListParamModel:
+            trainModelGlove(X=self.X,Y=self.Y,model=self.ListParamModel,path=self.path,cv=self.cv,n_jobs=self.n_jobs)
+
+
+
+
+
+
+
+if __name__=="__main__":
+       print("Debut du programme")
+    
+    #testData=pd.read_csv("../../data/TestData.csv")
+    print("Clear data")
+    t1=time.time()
+    
+    
+    if not os.path.exists("../../data/TrainDataClean.csv"):
+        trainData=pd.read_csv("../../data/TrainData.csv")
+        XtrainClean=Cleardataset().fit(trainData.resume).transform(trainData.resume)
+        XtrainCleanPrim=Cleardataset().fit(trainData.description).transform(trainData.description)
+        trainData.resume=XtrainClean
+        trainData.description=XtrainCleanPrim
+        trainData.to_csv("../../data/TrainDataClean.csv",index=False)
+    else:
+         trainData=pd.read_csv("../../data/TrainDataClean.csv")
+
+    print("data cleared in:{}".format(time.time()-t1))
+
+    path_to_modele="../data/"
+    demiLen=int(len(modelfinal)/2)
+    modelList1 = [modelfinal[i] for i in range((demiLen))]
+    modelList2 = [modelfinal[i] for i in range((demiLen),len(modelfinal))]
+
+    
+    Thread1 = BestModelFinder(X= trainData.resume,Y= trainData.Labels,ListParamModel=modelList1,path=path_to_modele)
+    Thread2 = BestModelFinder(X= trainData.resume,Y= trainData.Labels,ListParamModel=modelList2,path=path_to_modele)
+
+    print("Execute and save modele:")
+
+    Thread1.start()
+    Thread2.start()
+
+    Thread1.join()
+    Thread2.join()
